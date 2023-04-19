22311a600d07501bbaa159c2dcf7a779c6b8808c
Anartz Mugika Ledo <mugan86@gmail.com>
2023-04-19 20:12:17 +0200

    feat: finish code

---
+++ src/integrations/react/date-picker/react.tsx
@@ -0,0 +1,22 @@
+/** @jsxImportSource react */
+import { format } from 'date-fns';
+import { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+
+export function DayPickerReact() {
+  const [selected, setSelected] = useState<Date>();
+
+  let footer = <p>Please pick a day.</p>;
+  if (selected) {
+    footer = <p>You picked {format(selected, 'PP')}.</p>;
+  }
+  return (
+    <DayPicker
+      mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      footer={footer}
+    />
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,18 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { QButton, QDisplay } from "~/integrations/react/button-display";
+import { QDayPickerReact } from "~/integrations/react/date-picker";
+import dayPickerStyles from "./../../node_modules/react-day-picker/dist/style.css?inline";
+export const DayPickerQwik = component$(() => {
+  useStylesScoped$(dayPickerStyles);
+  return <QDayPickerReact />;
+});
 
 export default component$(() => {
-  console.log('Qwik Render');
-  const count = useSignal(0);
   return (
     <div class="section">
       <div class="container center">
-        <h2>Comunicación con dos componentes de React</h2>
-          <QButton
-          onClick$={() => {
-            console.log('click', count.value);
-            count.value++;
-          }}
-        />
-        <QDisplay count={count.value} />
+        <h2>Trabajando con un paquete NPM</h2>
+        <DayPickerQwik />
       </div>
     </div>
   );
